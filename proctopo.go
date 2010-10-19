c678d6c3e7a5f09722057c452d81a8e17b49a400
jpoirier <>
2010-10-19 18:13:27 -0500

    uses init function and globals now, misc comment changes

--- proctopo.go
+++ proctopo.go
@@ -12,20 +12,18 @@ import (
 	"unsafe"
 )
 
-type ProcTopo_t struct {
-	CpuidPresent    bool
-	CpuidRestricted bool
-	HttSupported    bool
-	Vendor          string
-	MaxProc         uint32
-	OSProcCnt       uint32
-	PkgCntEnum      uint32
-	CoreCntEnum     uint32
-	ThreadCntEnum   uint32
-	HttSmtPerCore   uint32
-	HttSmtPerPkg    uint32
-	Error           bool
-}
+var CpuidPresent    bool
+var CpuidRestricted bool
+var HttSupported    bool
+var Vendor          string
+var MaxProc         uint32
+var OSProcCnt       uint32
+var PkgCntEnum      uint32
+var CoreCntEnum     uint32
+var ThreadCntEnum   uint32
+var HttSmtPerCore   uint32
+var HttSmtPerPkg    uint32
+var Error           bool
 
 type regs struct {
 	eax uint32
@@ -66,71 +64,60 @@ func utos(a uint32) string {
 	return fmt.Sprintf("%s", b)
 }
 
-// cpuParams
-func cpuParams(s *ProcTopo_t) bool {
-	s.PkgCntEnum = 1
-	s.MaxProc = Conf()
-
-	//----------------------------
+// CpuParams
+func CpuParams() bool {
+	PkgCntEnum    = 1
+	CoreCntEnum   = 1
+	ThreadCntEnum = 1
+	HttSmtPerCore = 0
+	HttSmtPerPkg  = 0
+	MaxProc       = Conf()
+	OSProcCnt     = Onln()
 	// cpuid check
-	//----------------------------
-	s.CpuidPresent = have_cpuid()
-	if !s.CpuidPresent {
+	CpuidPresent = have_cpuid()
+	if !CpuidPresent {
 		return false
 	}
-
-	//----------------------------
 	// vendor name
-	//----------------------------
 	var info regs
 	cpuid(&info, 0, 0)
 	maxCpuid := info.eax
-	s.Vendor = utos(info.ebx) + utos(info.edx) + utos(info.ecx)
-
-	//----------------------------
+	Vendor = utos(info.ebx) + utos(info.edx) + utos(info.ecx)
 	// restricted cpuid execution
-	//----------------------------
 	var r regs
-	s.CpuidRestricted = false
+	CpuidRestricted = false
 	cpuid(&r, 0x80000000, 0)
 	if maxCpuid<=4 && r.eax>0x80000004 {
-		s.CpuidRestricted = true
+		CpuidRestricted = true
 		return false
 	}
-
-	//----------------------------
 	// htt enabled
-	//----------------------------
-	s.HttSupported = false
+	HttSupported = false
 	cpuid(&r, 1, 0)
 	if r.edx>>28&1 != 0 {
-		s.HttSupported = true
+		HttSupported = true
 	}
-
-	//----------------------------
-	// physical and logical core cnt
-	//----------------------------
+	// physical and logical core cnt for this package
 	var logCoreCnt uint32 = r.ebx >> 16 & 0xFF
 	var phyCoreCnt uint32
-	if s.Vendor == "GenuineIntel" {
+	if Vendor == "GenuineIntel" {
 		cpuid(&r, 4, 0)
 		phyCoreCnt = (r.eax >> 26 & 0x3F) + 1
-	} else if s.Vendor == "AuthenticAMD" {
+	} else if Vendor == "AuthenticAMD" {
 		cpuid(&r, 0x80000008, 0)
 		phyCoreCnt = (r.ecx & 0xFF) + 1
 	} else {
-		s.Error = true
+		Error = true
 		return false
 	}
-	s.PkgCntEnum = s.MaxProc - logCoreCnt + 1 // wrong? symmetrical for multiple packages ?
-	s.CoreCntEnum = phyCoreCnt
-	s.ThreadCntEnum = logCoreCnt
-	s.HttSmtPerPkg = logCoreCnt - phyCoreCnt
-	if logCoreCnt > phyCoreCnt { s.HttSmtPerCore = 1 /* always 1? */ }
+	PkgCntEnum    = MaxProc / logCoreCnt // wrong? symmetrical for multiple packages?
+	CoreCntEnum   = phyCoreCnt
+	ThreadCntEnum = logCoreCnt
+	HttSmtPerPkg  = logCoreCnt - phyCoreCnt
+	if logCoreCnt > phyCoreCnt && HttSupported { HttSmtPerCore = 1 /* always 1? */ }
 	return true
 }
 
-// ProcTopo
-func ProcTopo(s *ProcTopo_t) {
-	cpuParams(s)
+func init() {
+	CpuParams()
 }
