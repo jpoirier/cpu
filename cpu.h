03c35666a5c3824eac7ce2f6a9452bfcf92ee76b
jpoirier <>
2011-01-31 23:05:28 -0600

    test commit

--- cpu.h
+++ cpu.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2010 Joseph D Poirier
 // Distributable under the terms of The New BSD License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file. 
 
 #ifndef CPU_H
 #define CPU_H
